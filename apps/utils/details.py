12a8e94deaf0e7d4d5d7f4ac25a2d45dd325b426
SunnatDevPy <stalker.yoqubov@gmail.com>
2024-12-10 23:02:29 +0500

    add

--- apps/utils/details.py
+++ apps/utils/details.py
@@ -2,7 +2,7 @@ from apps.models import Cart, Product, Category, Shop, User
 
 
 async def sum_from_shop(shop_id, user):
-    carts: list['Cart'] = await Cart.get_cart_from_shop(shop_id, user.id)
+    carts: list['Cart'] = await Cart.get_cart_from_shop(user.id, shop_id)
     sum_ = 0
     for i in carts:
         product: 'Product' = await Product.get(i.product_id)
@@ -17,7 +17,7 @@ async def sum_from_shop(shop_id, user):
 
 async def detail_cart(shop_id, user_id):
     user = await User.get(user_id)
-    carts: list['Cart'] = await Cart.get_cart_from_shop(shop_id, user.id)
+    carts: list['Cart'] = await Cart.get_cart_from_shop(user.id, shop_id)
     cart_ = []
     for i in carts:
         sum_ = 0
@@ -41,6 +41,33 @@ async def detail_cart(shop_id, user_id):
             "sum": sum_
         })
     return cart_
+
+# async def detail_cart2(shop_id, user_id):
+#     user = await User.get(user_id)
+#     carts: list['Cart'] = await Cart.get_cart_from_shop(shop_id, user.id)
+#     cart_ = []
+#     for i in carts:
+#         sum_ = 0
+#         product: 'Product' = await Product.get(i.product_id)
+#         if user.type == 'optom':
+#             sum_ += i.count * product.optom_price
+#             price = product.optom_price
+#         elif user.type == 'restorator':
+#             sum_ += i.count * product.restorator_price
+#             price = product.restorator_price
+#         else:
+#             sum_ += i.count * product.one_price
+#             price = product.one_price
+#         cart_.append({
+#             'id': i.id,
+#             "user_id": i.user_id,
+#             "shop_id": i.shop_id,
+#             "product_id": i.product_id,
+#             "count": i.product_id,
+#             "price": price,
+#             "sum": sum_
+#         })
+#     return cart_
 
 
 async def get_products_utils(shop_id):
