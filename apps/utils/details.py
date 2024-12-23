5b565ad66d40d18c455882741b21896fb021b8b9
SunnatDevPy <stalker.yoqubov@gmail.com>
2024-12-23 19:56:13 +0500

    add

--- apps/utils/details.py
+++ apps/utils/details.py
@@ -89,7 +89,10 @@ async def get_shops_unique_cart(carts):
     unique_ids = set()
     unique_cart = []
     for i in carts:
-        shop = await Shop.get(i['shop_id'])
+        try:
+            shop = await Shop.get(i.shop_id)
+        except:
+            shop = await Shop.get(i['shop_id'])
         user = await User.get(i['user_id'])
         sum_ = await sum_from_shop(shop.id, user)
         if shop.id not in unique_ids:
